edb0e1bd3b3e400221b7bc85d3ead2463480acda
Filipe Carvalho <filipeferc88@gmail.com>
2024-05-03 01:30:55 +0100

    Added requirements into the iris_project.ipynb Created python file analysis.py

---
+++ analysis.py
@@ -0,0 +1,7 @@
+# Program: analysis.py
+# This program:
+#   1. Outputs a summary of each variable to a single text file,
+#   2. Saves a histogram of each variable to png files, and
+#   3. Outputs a scatter plot of each pair of variables.
+#   4. Performs any other analysis you think is appropriate.
+# Author: Filipe Carvalho
