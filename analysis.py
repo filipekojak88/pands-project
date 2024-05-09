e62e9a0d5270b15e37ab5f6221aa53d0f1661c6d
Filipe Carvalho <filipeferc88@gmail.com>
2024-05-09 22:43:28 +0100

    Added iris.data to the repository Loaded the iris dataset into analysis.py Started off with reading iris dataset and outputing a file called summary.txt. Added references to the Jupyter notebook

--- analysis.py
+++ analysis.py
@@ -5,3 +5,26 @@
 #   3. Outputs a scatter plot of each pair of variables.
 #   4. Performs any other analysis you think is appropriate.
 # Author: Filipe Carvalho
+
+# Part 1: Outputs a summary of each variable to a single text file
+
+# Import os to check if the file exists
+import os
+
+# Assign the iris dataset to a variable
+file_name = 'iris.data'
+
+# Check if file exists
+if os.path.exists(file_name):
+    # if file exists, then load the iris dataset
+    with open (file_name, 'r') as df:
+        data = df.read()
+else:
+    # if file does not exist, then output a message
+    print(f'{file_name} does not exist')
+
+
+# Output to a single text file
+with open ('summary.txt','wt') as sf:
+    sf.write('summary')
+# print ('done')
