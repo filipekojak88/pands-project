0c2ce8958b2980c17712e4834408f9e347bb1ad8
Filipe Carvalho <filipeferc88@gmail.com>
2024-05-11 00:15:36 +0100

    Adding

--- analysis.py
+++ analysis.py
@@ -8,21 +8,31 @@
 
 # Part 1: Outputs a summary of each variable to a single text file
 
-# Import os to check if the file exists
+# Import 'os' to check if the file exists
 import os
+# Import 'pandas' for the data summary
+import pandas as pd
 
 # Assign the iris dataset to a variable
 file_name = 'iris.data'
 
 # Check if file exists
 if os.path.exists(file_name):
-    # if file exists, then load the iris dataset
-    with open (file_name, 'r') as df:
-        data = df.read()
+    # if file exists, then load the iris dataset using pandas
+    df = pd.read_csv(file_name, header=None)
 else:
     # if file does not exist, then output a message
-    print(f'{file_name} does not exist')
+    print(f'{file_name} does not exist. The "iris.data" file needs to be saved in the repository pands-project')
 
+# Adding Column titles to the iris.data
+# Defining the column titles
+column_title = ["sepal length in cm", "sepal width in cm", "petal length in cm", "petal width in cm", "class"]
+
+# Setting column titles as headers to the dataframe
+df.columns = column_title
+
+# Print the first 5 rows of the DataFrame
+print(df.head())
 
 # Output to a single text file
 with open ('summary.txt','wt') as sf:
