c23605fa85f5a96ee0385c131afb03641cb43941
Filipe Carvalho <filipeferc88@gmail.com>
2024-05-11 15:29:16 +0100

    Organizing the headings on the summary.txt file

--- analysis.py
+++ analysis.py
@@ -12,6 +12,8 @@
 import os
 # Import 'pandas' for the data summary
 import pandas as pd
+# Import 'io' to use a string buffer for df.info()
+import io
 
 # Assign the iris dataset to a variable
 file_name = 'iris.data'
@@ -26,7 +28,7 @@ else:
 
 # Adding Column titles to the iris.data
 # Defining the column titles
-column_title = ["sepal length in cm", "sepal width in cm", "petal length in cm", "petal width in cm", "class"]
+column_title = ["Sepal Length (cm)", "Sepal Width (cm)", "Petal Length (cm)", "Petal Width (cm)", "Species"]
 
 # Setting column titles as headers to the dataframe
 df.columns = column_title
@@ -36,6 +38,12 @@ df.columns = column_title
 
 
 # Summary of the variables
+
+# Types of Variables of the Dataset
+# Creating a string buffer to capture the output
+variable_buffer = io.StringIO()
+# Capturing the output of df.info()
+df.info(buf=variable_buffer)
 
 # Categorical Data
 categorical_summary = {}
@@ -60,21 +68,37 @@ continuous_summary = df.describe()
 # Output to a single text file
 with open('summary.txt', 'wt') as sf:
     # Writing the title of the summary.txt file
-    sf.write("This is the summary of the variables in Iris Dataset:\n\n")
+    sf.write("This is a summary of the Iris Dataset variables:\n\n\n")
+
+    # Introducing the types of variables in the dataset
+    sf.write('1. Introduction - Summary of the Data Types in Python:\n\n')
+    # Writing the captured output of df.info() to the file
+    sf.write(variable_buffer.getvalue())
+    # Writing the variable types and their corresponding types
+    sf.write('\n\n2.Variables Types Classification based on Python Data Types: \n\nobject = Categorical Variable \nfloat64 = Continuous Variable')
+
+
+    # Write summary for categorical variables
+    sf.write("\n\n\n3. Summary for Categorical Variables:\n\n")
+    # Initialize the counter
+    counter = 1
+    for variable, summary in categorical_summary.items():
+        # Write the variable name before its summary
+        sf.write(f"3.{counter} Variable: {variable}\n")
+        sf.write(summary['count'].to_string(header=False))
+        sf.write(f"\n\nUnique Categories: {summary['unique_categories']}\n\n\n")
+        # Iterating in each loop
+        counter += 1 
 
     # Write summary for continuous variables
-    sf.write("1. Summary for Continuous Variables:\n\n")
+    sf.write("4. Summary for Continuous Variables:\n\n")
     # Initialize the counter
     counter = 1
     for column in continuous_summary.columns:
-        sf.write(f"1.{counter} Variable: {column}\n")
+        sf.write(f"4.{counter} Variable: {column}\n")
         for statistic in continuous_summary.index:
             sf.write(f"{statistic.capitalize()}: {continuous_summary.loc[statistic, column]}\n")
         sf.write("\n")
+        # Iterating in each loop
         counter += 1 
-    # Write summary for categorical variables
-    sf.write("2. Summary for Categorical Variables:\n")
-    for variable, summary in categorical_summary.items():
-        sf.write(f"Variable: {variable}\n")
-        sf.write(summary['count'].to_string())
-        sf.write(f"\nUnique Categories: {summary['unique_categories']}\n\n")
+   
